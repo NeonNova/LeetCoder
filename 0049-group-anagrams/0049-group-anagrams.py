c4c642143db878cc9e010792f1991cbc1307fcbc
Aditya Washikar <69235477+NeonNova@users.noreply.github.com>
2024-06-06 12:40:27 +0530

    Time: 91 ms (50.66%), Space: 22.2 MB (22.94%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        res = defaultdict(list)
+
+        for i in strs:
+            count = [0]*26
+
+            for c in i:
+                count[ord(c) - 97] += 1
+            
+            res[tuple(count)].append(i)
+        
+        return res.values()
+        
