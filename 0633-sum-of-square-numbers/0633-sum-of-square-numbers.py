68d6ba9c2bcc5ce48d3c8dc860db72ec8d399943
Aditya Washikar <69235477+NeonNova@users.noreply.github.com>
2024-06-17 20:36:16 +0530

    Time: 127 ms (26.2%), Space: 16.6 MB (39.72%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -3,13 +3,13 @@ import math
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
         # Define the limit as the square root of c
-        limit = math.isqrt(c)
+        #limit = math.isqrt(c)
 
         # Initialize two pointers
-        low, high = 0, limit
+        low, high = 0, math.isqrt(c)
 
         while low <= high:
-            sum_of_squares = low * low + high * high
+            sum_of_squares = low ** 2 + high ** 2
             if sum_of_squares == c:
                 return True
             elif sum_of_squares < c:
